63b6349474881197a6c0e0a624508549bd484153
speed-i <154674790+speed-i@users.noreply.github.com>
2023-12-23 19:43:51 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*
+* Purpose		    : This file is designed to XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+*                     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+* Organization  	: 
+* Author		    : 
+* Last Modified		: 15/12/2023
+* Version		    : 1.0.0
+*/
+
+/**
+ * This function is to XXX
+ * @param {string} type The type of XXX
+ * @returns {void} This function does not return anything.
+ */
+
+// Codes Start Here.
---
+++ js/services.js
@@ -0,0 +1,46 @@
+"use strict"
+/*
+* Purpose		    : This file is designed to use for the API calls to the various data APIs. It is to
+*                     be included in the HEAD element of HTML pages.
+* Organization  	: Monash University Malaysia
+* Author		    : Teo Khay Yi
+* Last Modified		: 16 July 2021
+* Version		    : 1.0.0
+*/
+
+/**
+ * This function is to call various data APIs.
+ * @param {string} url URL of API Web Service Request
+ * @param {object} data containing data to be sent via the query string using key:value properties
+ * @returns {void} This function does not return anything.
+ */
+function webServiceRequest(url,data)
+{
+	// Build URL parameters from data object.
+    let params = "";
+    // For each key in data object...
+    for (let key in data)
+    {
+        if (data.hasOwnProperty(key))
+        {
+            if (params.length == 0)
+            {
+                // First parameter starts with '?'
+                params += "?";
+            }
+            else
+            {
+                // Subsequent parameter separated by '&'
+                params += "&";
+            }
+
+            let encodedKey = encodeURIComponent(key);
+            let encodedValue = encodeURIComponent(data[key]);
+
+            params += encodedKey + "=" + encodedValue;
+         }
+    }
+    let script = document.createElement('script');
+    script.src = url + params;
+    document.body.appendChild(script);
+}
